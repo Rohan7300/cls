fb70cd6a79a31d8f7c41d5288df8b83d9c95460e
devv Expert <56317004+devv-expert@users.noreply.github.com>
2024-03-20 11:04:32 +0530

    UI changes

--- app/src/main/java/com/clebs/celerity/fragments/Userprofile.kt
+++ app/src/main/java/com/clebs/celerity/fragments/Userprofile.kt
@@ -5,23 +5,21 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import com.clebs.celerity.Factory.MyViewModelFactory
 import com.clebs.celerity.R
 import com.clebs.celerity.ViewModel.MainViewModel
 import com.clebs.celerity.databinding.FragmentUserprofileBinding
 import com.clebs.celerity.models.requests.UpdateProfileRequestBody
-
 import com.clebs.celerity.network.ApiService
 import com.clebs.celerity.network.RetrofitService
 import com.clebs.celerity.repository.MainRepo
