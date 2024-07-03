bdf0890c38a4083ac6061605a8524c24e095146c
devv Expert <56317004+devv-expert@users.noreply.github.com>
2024-07-03 18:58:03 +0530

    Merge branch 'sagarUpdated'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/clebs/celerity/network/RetrofitService.kt

--- app/src/main/java/com/clebs/celerity/ViewModel/ImageViewModelProvider.kt
+++ app/src/main/java/com/clebs/celerity/ViewModel/ImageViewModelProvider.kt
@@ -4,14 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.clebs.celerity.database.ImagesRepo
 import com.clebs.celerity.ui.App
-import io.clearquote.assessment.cq_sdk.support.dateFormat
+import com.clebs.celerity.utils.dateToday
+
 import java.util.Date
 
 class ImageViewModelProviderFactory(
     val imagesRepo: ImagesRepo
 ) :ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val todayDate = dateFormat.format(Date())
+        val todayDate = dateToday()
         return ImageViewModel(imagesRepo,todayDate) as T
     }
 }
